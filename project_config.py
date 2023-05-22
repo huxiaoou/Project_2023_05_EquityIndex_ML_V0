ecf037538ec070ce3aeba7bc5709c39ae8cff741
huxiaoou <516984451@qq.com>
2023-05-22 17:15:24 +0800

    Tiny improvments

--- project_config.py
+++ project_config.py
@@ -48,26 +48,25 @@ y_lbls = ["rtm"]
 instruments_universe = ["IC.CFE", "IH.CFE", "IF.CFE", "IM.CFE"]
 tids = ["T{:02d}".format(t) for t in range(1, 8)]
 train_windows = (6, 12, 24)
-for instrument, tid in ittl.product(instruments_universe + [None], tids + [None]):
+model_lbls = ["lm", "mlpc", "mlpr"]
+for instrument, tid, trn_win, model_lbl in ittl.product(
+        instruments_universe + [None], tids + [None], train_windows, model_lbls):
     model_grp_id = "-".join(["M"] + list(filter(lambda z: z, [instrument, tid])))
-    for trn_win in train_windows:
-        for model_lbl in ["lm", "nn"]:
-            pred_id = model_grp_id + "-TMW{:02d}".format(trn_win) + "-pred-{}".format(model_lbl)
-            sqlite3_tables.update({
-                pred_id: {
-                    "table_name": "predictions",
-                    "primary_keys": {
-                        "trade_date": "TEXT",
-                        "instrument": "TEXT",
-                        "contract": "TEXT",
-                        "tid": "TEXT",
-                        "timestamp": "INT4",
-                    },
-                    "value_columns": {
-                        "rtm": "REAL",
-                        "pred": "REAL",
-                    }
-                },
-            })
-
+    pred_id = model_grp_id + "-TMW{:02d}".format(trn_win) + "-pred-{}".format(model_lbl)
+    sqlite3_tables.update({
+        pred_id: {
+            "table_name": "predictions",
+            "primary_keys": {
+                "trade_date": "TEXT",
+                "instrument": "TEXT",
+                "contract": "TEXT",
+                "tid": "TEXT",
+                "timestamp": "INT4",
+            },
+            "value_columns": {
+                "rtm": "REAL",
+                "pred": "REAL",
+            }
+        },
+    })
 cost_rate = 5e-4
